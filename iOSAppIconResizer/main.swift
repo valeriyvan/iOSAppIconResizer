1ffdedb2938cc5f1c043073c2b3df37d739bc73d
Valeriy Van <valeriyvan@gasts-macbook-pro.local>
2018-07-03 13:04:28 +0200

    Initial Commit

---
+++ iOSAppIconResizer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iOSAppIconResizer
+//
+//  Created by Valeriy Van on 03/07/2018.
+//  Copyright © 2018 Valeriy Van. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
